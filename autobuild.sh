ea8c5937c787954afc6ae425e5c2e983f45db422
AntimatterReactor <ezraalvarion71@gmail.com>
2022-03-26 21:07:03 +0700

    Added flags to Release and Debug Builds of CMake

--- autobuild.sh
+++ autobuild.sh
@@ -1,5 +1,7 @@
 mkdir -v -p build/{release,debug}
-cmake -B build/debug -S . -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Debug
-cmake -B build/release -S . -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release
+cmake -B build/debug -S . -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Debug \
+-DCMAKE_CXX_COMPILER=g++
+cmake -B build/release -S . -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release \
+-DCMAKE_CXX_COMPILER=g++
 make -j4 -C build/debug
 make -j4 -C build/release
